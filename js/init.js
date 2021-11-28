c26194f39a60d90758cfdd20ac1a26a2ab20eab2
AngelRod5il <95190252+AngelRod5il@users.noreply.github.com>
2021-11-28 16:10:50 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,15 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyB84ulrVqdb_k90Znp_vIGFXXPG60LJQcI",
+
+  authDomain: "pagina-cloud-storage.firebaseapp.com",
+
+  projectId: "pagina-cloud-storage",
+
+  storageBucket: "pagina-cloud-storage.appspot.com",
+
+  messagingSenderId: "445925622407",
+
+  appId: "1:445925622407:web:5882b4456fad12ee3cae22",
+
+  measurementId: "G-J9PW51N9ZT"
+});
